22ac8deec10cf0848d9e4d8137616b15f9062d57
mstone-gdsnet <61155808+mstone-gdsnet@users.noreply.github.com>
2020-11-12 11:40:53 -0300

    Update apiSalas.js

--- apis_app/apiSalas.js
+++ apis_app/apiSalas.js
@@ -33,36 +33,33 @@ exports.funSalas = function (req, res)  {
             conn.close();
         };   
 
-    }
 
-  async function funcionQuery  (queryspdv, conn) {
-    return await conn.query(queryspdv).then( (res_sql) => {
-    //console.info("va con todo: ", JSON.stringify(res_sql.recordsets[2]))
-    return new Promise((resolve) => {
-        resolve(funAgruparData(res_sql.recordsets))
-    }).then(respuesta=>{
-        //console.log('respuesta OK', respuesta)
-        return respuesta
-      })
-      .catch(respuesta=>{
-       console.log('respuesta error', respuesta)
-      })
-  })
-  }
-  
-
-
-  function funAgruparData (data) {
+        async function funcionQuery  (queryspdv, conn) {
+          return await conn.query(queryspdv).then( (res_sql) => {
+          //console.info("va con todo: ", JSON.stringify(res_sql.recordsets[2]))
+          return new Promise((resolve) => {
+              resolve(funAgruparData(res_sql.recordsets))
+          }).then(respuesta=>{
+              //console.log('respuesta OK', respuesta)
+              return respuesta
+            })
+            .catch(respuesta=>{
+             console.log('respuesta error', respuesta)
+            })
+        })
+        }
+        
+        function funAgruparData (data) {
    
-    salas = data[0]
-    indicadores = data[1]
-    detalles = data[2]
-    // console.log("recibiendo todo el salas :  : ", salas)
-    // console.log("recibiendo todo el indicadores :  : ", indicadores)
-    // console.log("recibiendo todo el detalles :  : ", detalles)
-    return funAgrupando(salas, indicadores, detalles)
-  }
-    
+          salas = data[0]
+          indicadores = data[1]
+          detalles = data[2]
+          return funAgrupando(salas, indicadores, detalles)
+        }
+          
+        
+
+    }
 
 
    function  funAgrupando (salas, indicadores, detalles) {
@@ -71,13 +68,11 @@ exports.funSalas = function (req, res)  {
      const  dataIndicador  =  indicadores
      .filter(indicadores => {
        if(indicadores.id_sala===val.id_sala){
-
        dataDetalle = detalles.filter(det => {
           if(indicadores.id_sala===det.id_sala && indicadores.id_indicador===det.id_indicador ){
           return det
           }
         })
-      
         return indicadores["detalles"] = dataDetalle
        }
      })
